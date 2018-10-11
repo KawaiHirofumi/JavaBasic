f49fc2d18621836b995e2a767f820cff4641ad12
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-11 18:20:35 +0900

    2－10を終了いたしました！

--- Practice/Practice/src/practice02/PTra02_10.java
+++ Practice/Practice/src/practice02/PTra02_10.java
@@ -8,14 +8,17 @@ package practice02;
 
 public class PTra02_10 {
 	public static void main(String[] args) {
-		int moneyInHand = 1000;	// 所持金
-		int coffee = 120;	// コーヒーの値段
-		int snack = 200;	// スナック菓子
+		int moneyInHand = 1000; // 所持金
+		int coffee = 120; // コーヒーの値段
+		int snack = 200; // スナック菓子
+		int buycoffee = (coffee * 2);
+		int much = (buycoffee+snack);
+		int result = (moneyInHand - (int)(much *1.08) );
 
+		System.out.println(result+"円");
 		/*
 		 * ●一本120円（税別）の缶コーヒーを2本
 		 * ●一袋200円（税別）のスナック菓子を1袋
-		 *
 		 * ★ 上記の商品を購入したときの所持金を出力してください
 		 * ※ 消費税は、全商品の合計に8%が加算されます
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
