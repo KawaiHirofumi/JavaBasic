b996c717e0a685fdd2720bd57b055e396e77a8f9
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-19 13:47:30 +0900

    6- 4を終了した！

--- Practice/Practice/src/practice06/PTra06_04.java
+++ Practice/Practice/src/practice06/PTra06_04.java
@@ -9,6 +9,12 @@ package practice06;
 public class PTra06_04 {
 	public static void main(String[] args) {
 
+
+		for(int i=0; i<5; i++) 
+			
+            System.out.print("■");
+		
+		
 		// ★ for文を使って、以下のような出力になるようにしてください
 		/*
 		 * 出力結果例
