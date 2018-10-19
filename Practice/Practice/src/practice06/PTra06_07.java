4e333bedb2dd4a0884f3faaa7926f5bcea2c31e3
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-19 13:49:59 +0900

    6‐7を終了した！

--- Practice/Practice/src/practice06/PTra06_07.java
+++ Practice/Practice/src/practice06/PTra06_07.java
@@ -9,21 +9,67 @@ package practice06;
 public class PTra06_07 {
 	public static void main(String[] args) {
 
-		// ★ for文を使って、以下のような出力になるようにしてください
-		/*
-		 * 出力結果例
-		 * -----------------------------------
-		 * □□□□□
-		 * ■□□□□
-		 * ■■□□□
-		 * ■■■□□
-		 * ■■■■□
-		 * ■■■■■
-		 * ■■■■□
-		 * ■■■□□
-		 * ■■□□□
-		 * ■□□□□
-		 */
+		int count = 0;
+	    for (int i = 0; i < 5; i++) {
+	        String test = "";
+	        for (int j = 0; j < count; j++) {
+	            test += "■";
+	        }
+	        for (int k = 5 - count; k > 0; k--) {
+	            test += "□";
+	        }
+	        System.out.println(test);
+	        count++;
+	    }
 
+	    count = 0; // 初期化
+	    for (int i = 0; i < 5; i++) {
+	        String test = "";
+	        for (int k = 5 - count; k > 0; k--) {
+	            test += "■";
+	        }
+	        for (int j = 0; j < count; j++) {
+	            test += "□";
+	        }
+	        System.out.println(test);
+	        count++;
+	    }
 	}
 }
+
+// ★ for文を使って、以下のような出力になるようにしてください
+/*
+ * 出力結果例
+ * -----------------------------------
+ * □□□□□
+ * ■□□□□
+ * ■■□□□
+ * ■■■□□
+ * ■■■■□
+ * ■■■■■
+ * ■■■■□
+ * ■■■□□
+ * ■■□□□
+ * ■□□□□
+ */
+
+
+//
+//int count = 0;
+//for (int x = 0; x < 10; x++) {
+//    for (int y = 0; y < 5; y++) {
+//        if (x - y <= 0) {
+//            System.out.print("□");
+//        }
+//        else if (x < 5) {
+//            System.out.print("■");
+//        }
+//        else if (x + y < 10) {
+//            System.out.print("■");
+//        }
+//        else {
+//            System.out.print("□");
+//        }
+//    }
+//    System.out.println();
+//}
