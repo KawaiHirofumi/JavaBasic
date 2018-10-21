533595a86a741a66c249b4f0a62d20c719a294c8
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-21 21:24:35 +0900

    7－2を終了した！

--- Practice/Practice/src/practice07/PTra07_02.java
+++ Practice/Practice/src/practice07/PTra07_02.java
@@ -1,4 +1,60 @@
 package practice07;
+
+public class PTra07_02 {
+	public static void main(String[] args) {
+
+		// ★ int型の配列arrayを宣言し、1000個の領域で確保してください
+
+		int[] array = new int[1000];
+
+		// ★ 配列arrayに [1,2,3,4,5,6,7,・・・・,998,999,1000] を代入してください
+
+		for (int i = 0; i < 1000; i++) {
+			array[i] = i+1;
+		}
+
+		////////////////////////////////
+
+		for (int i = 0; i < 1000; i++) {
+			System.out.println(array[i]);
+
+			// ★ 変数arrayに格納されている値を前から順番に全て表示してください
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  * PTra07_02.java
  *   作成	LIKEIT	2017
@@ -6,16 +62,3 @@ package practice07;
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 
-public class PTra07_02 {
-	public static void main(String[] args) {
-
-		// ★ int型の配列arrayを宣言し、1000個の領域で確保してください
-
-
-		// ★ 配列arrayに [1,2,3,4,5,6,7,・・・・,998,999,1000] を代入してください
-
-
-		// ★ 変数arrayに格納されている値を前から順番に全て表示してください
-
-	}
-}
