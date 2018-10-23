a6739b8fd4b72a68d2871ea4808aaf8cbd551d54
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-23 10:15:57 +0900

    8－1を終了した

--- Practice/Practice/src/practice08/PTra08_01.java
+++ Practice/Practice/src/practice08/PTra08_01.java
@@ -11,8 +11,11 @@ public class PTra08_01 {
 
 		// ★ 変数receiveを宣言し、メソッドnumberを実行した結果を代入してください
 
+		int receive = number();
 
 		// ★ 変数receiveを出力してください
+
+		System.out.println(receive);
 
 	}
 
@@ -25,7 +28,9 @@ public class PTra08_01 {
 	 *
 	 * 100を返すメソッド
 	 */
+
 	public static int number() {
 		return 100;
+
 	}
 }
