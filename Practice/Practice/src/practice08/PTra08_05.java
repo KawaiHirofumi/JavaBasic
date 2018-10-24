1d63a42a75dfe830239d083170411313f6352c37
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-24 13:23:34 +0900

    8－5を終了した

--- Practice/Practice/src/practice08/PTra08_05.java
+++ Practice/Practice/src/practice08/PTra08_05.java
@@ -16,12 +16,14 @@ public class PTra08_05 {
 
 		// コマンドプロンプトで入力した数値が変数widthに代入されます
 		// ※ コマンドプロンプトで入力された値が、数字ではなかった場合はプログラムがエラーになります
+
 		int width = scanner.nextInt();
 
 		System.out.println("図形の縦の長さを入力してください");
 
 		// コマンドプロンプトで入力した数値が変数heightに代入されます
 		// ※ コマンドプロンプトで入力された値が、数字ではなかった場合はプログラムがエラーになります
+
 		int height = scanner.nextInt();
 
 		System.out.println("図解の形を選択してください");
@@ -29,10 +31,16 @@ public class PTra08_05 {
 
 		// コマンドプロンプトで入力した数値が変数heightに代入されます
 		// ※ コマンドプロンプトで入力された値が、数字ではなかった場合はプログラムがエラーになります
+
 		int choice = scanner.nextInt();
 
 		// ★ 変数choiceの中が、1であれば四角形の面積を算出するメソッドを、2であれば三角形の面積を算出するメソッドを呼び出してください
 
+		if(choice == 1) {
+			System.out.println(quadrangle(width, height));
+		}else if(choice == 2) {
+			System.out.println(triangle(width, height));
+		}
 	}
 
 	/*
@@ -46,6 +54,10 @@ public class PTra08_05 {
 	 */
 
 
+	public static double quadrangle(double width, double height) {
+		return width * height;
+	}
+
 	/*
 	 * ★ 以下の仕様でメソッドを作成してください
 	 *
@@ -56,4 +68,8 @@ public class PTra08_05 {
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、三角形の面積を返すメソッド
 	 */
 
+	public static double triangle(double width, double height) {
+		return width * height / 2;
+
+}
 }
