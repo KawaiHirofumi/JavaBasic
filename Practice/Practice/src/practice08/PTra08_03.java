38375c610b766668960ca81a0a47acd359d7740f
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-10-24 13:23:05 +0900

    8－3を終了した

--- Practice/Practice/src/practice08/PTra08_03.java
+++ Practice/Practice/src/practice08/PTra08_03.java
@@ -11,9 +11,13 @@ public class PTra08_03 {
 
 		// ★ 0～100のランダムな値を取得して、変数numberに代入してください
 
+		int number=new java.util.Random().nextInt(100);
+		System.out.println(number);
 
 		// ★ 変数resultを宣言し、メソッドisEvenNumberを（変数number）で呼び出した結果を代入してください
 
+
+		Boolean result = isEvenNumber(number);
 
 		if (result) {
 			System.out.println("偶数です");
@@ -31,4 +35,38 @@ public class PTra08_03 {
 	 *
 	 * 引数で渡された内容が偶数ならtrue、奇数ならfalseを返すメソッド
 	 */
-}
+
+	public static boolean isEvenNumber(int x) {
+		return x%2==0?true:false;
+		}
+	}
+
+
+
+
+//public class PTra08_03 {
+//	public static void main(String[] args) {
+//
+//		// ★ 0～100のランダムな値を取得して、変数numberに代入してください
+//
+//
+//		// ★ 変数resultを宣言し、メソッドisEvenNumberを（変数number）で呼び出した結果を代入してください
+//
+//
+//		if (result) {
+//			System.out.println("偶数です");
+//		} else {
+//			System.out.println("奇数です");
+//		}
+//	}
+//
+//	/*
+//	 * ★ 以下の仕様でメソッドを作成してください
+//	 *
+//	 * 戻り値		：boolean
+//	 * メソッド名	：isEvenNumber
+//	 * 引数			：int x
+//	 *
+//	 * 引数で渡された内容が偶数ならtrue、奇数ならfalseを返すメソッド
+//	 */
+//}
