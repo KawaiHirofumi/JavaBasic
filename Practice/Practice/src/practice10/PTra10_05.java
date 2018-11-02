9f50e52629fed216d5accca9f1aa543adc3bb2e5
DESKTOP-HUO29GF\fmfm <hirofumi.kawai.likeit@gmail.com>
2018-11-02 16:51:39 +0900

    10－5を終了した！

--- Practice/Practice/src/practice10/PTra10_05.java
+++ Practice/Practice/src/practice10/PTra10_05.java
@@ -27,19 +27,21 @@ public class PTra10_05 {
 
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
-
+		Car car = new Car();
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
-
+		car.serialNo = 1000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
-
+		car.color = "red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
-
+		car.gasoline = 50;
 
 		// 目的地までの距離
+		
 		final int distance = 300;
+
 
 		/*
 		 * ★ 変数carに格納されているインスタンスメソッドrunを使って、目的地まで進んでください
@@ -48,5 +50,60 @@ public class PTra10_05 {
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
 
+
+		int sum = 0;//（進んだ距離）
+
+		for(int n = 0; n < 51; n ++) {//ここでループを終わらす定義
+
+			
+			
+			//while (n >= 51) {
+			//n ++;
+
+			int random = car.run();//ここでランダムを定義
+
+			if(random == -1) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
+
+			sum += random;//ここでランダムの値を定義
+			
+			System.out.println(sum);//ここでランダムの累積をいう
+
+			if(sum >= distance) {//ここに目的地
+
+				//int sum = (n*number);
+				int x = 50 - n;//今あるガソリンー進んだ数
+
+				System.out.println("目的地にまで" + n + "時間かかりました。残りのガソリンは、" + x + "リットルです");
+				break;
+			}
+		}
 	}
 }
+
+
+
+//
+//
+//		int runresult = 0; //runした回数(進んだ回数)
+//		int runcount = 0 ; //走行距離(時間)
+//
+//		while(true) {
+//			int move = car.run();
+//			//１ずつガソリンは消費して１～１５進む
+//			runresult += move; //ガソリン消費量も１づつ加算
+//			runcount++ ; 	//↑に加えmoveするごとに走行距離を加算
+//			System.out.println(runresult);
+//
+//			if (move == -1) {  //ガス欠の場合に対する処理
+//				System.out.println("目的地に到達できませんでした");
+//				break;           //ガス欠なので動けない＝これ以上処理はできない
+//			}else if (runresult >= distance) { //runresultがdistanceについたら処理はおわり
+//				System.out.println("目的地にまで" + runcount +"時間かかりました。残りのガソリンは、" + car.gasoline + "リットルです");
+//				break;			//目的地についたのでこれで処理はおわり
+//			}
+//		}
+//	}
+//}
